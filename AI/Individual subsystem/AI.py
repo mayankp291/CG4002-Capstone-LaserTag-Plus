ecacdbe3e2570666d6589e64fcf2ae7383afe681
JL <tha.junlim@gmail.com>
2023-01-27 04:14:25 +0800

    Move location of files

---
+++ AI/Individual subsystem/AI.py
@@ -0,0 +1,56 @@
+import csv
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+NEURONS_HIDDEN_LAYER = [40, 35]
+INPUTS = 792 # 128 sensor readings * 6 sensor reading types + 4 extracted features * 6 sensor reading types
+OUTPUTS = 6
+EPOCHS = 20
+
+def load_training_data():
+    return
+
+def load_testing_data():
+    return
+
+def get_model():
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Dense(NEURONS_HIDDEN_LAYER[0], input_shape=(INPUTS,), activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.3))
+    
+    i = 1
+    while (i < len(NEURONS_HIDDEN_LAYER)):
+        model.add(tf.keras.layers.Dense(NEURONS_HIDDEN_LAYER[i], activation="relu"))
+        model.add(tf.keras.layers.Dropout(0.3))
+        i += 1
+    
+
+    model.add(tf.keras.layers.Dense(OUTPUTS, activation="softmax"))
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+def main():
+
+    model = get_model()
+    print(model.summary())
+    training_dataset, training_data_label = load_training_data()
+    testing_dataset, testing_data_label = load_testing_data()
+
+
+    # Train the neural network model
+    model.fit(training_dataset, training_data_label, epochs=EPOCHS)
+
+    # Evaluate how well model performs
+    model.evaluate(testing_dataset, testing_data_label, verbose=2)
+
+
+if __name__ == "__main__":
+    main()
