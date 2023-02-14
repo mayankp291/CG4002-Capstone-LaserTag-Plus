9f7216600958075f1e86a83638b3e7d1a1cdf81a
Mayank Panjiyara <revflash@LAPTOP-8712783.localdomain>
2023-02-14 14:52:53 +0800

    Add Game Engine code

--- External Comms/gameEngine.py
+++ External Comms/gameEngine.py
@@ -1,23 +1,13 @@
 # Ultra96 Server
-from socket import *
-import socketserver
-from Crypto import Random
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-import base64
-import multiprocessing
 from multiprocessing import Process, Queue
 import json
 import paho.mqtt.client as mqtt
-import time
 import threading
 import random
-
 
 imu_queue = Queue()
 action_queue = Queue()
 viz_queue = Queue()
-
 
 
 player_state = {
@@ -45,37 +35,6 @@ player_state = {
     }
 }
 
-# TCP Server to receive data from the Relay Laptops
-class Relay_Server(socketserver.BaseRequestHandler):
-    def handle(self):
-        cur_thread = threading.current_thread()
-        while True:
-            data = self.request.recv(1024).decode('utf-8')
-            # process data from client
-            data = data.strip()
-            data = data.split('_')
-            length = int(data[0])
-            
-
-            if length != len(data[2]):
-                print("Error", data)
-                print('Error: packet length does not match, packet dropped')
-            
-            else:
-                print("{} wrote:".format(self.client_address), data[2])
-                # process incoming data
-                imu_queue.put(data[2])
-                # if data[1] == 'action' and data[2] != 'none':
-                #     client_queue.put(data[2])
-                # elif data[1] == 'IMU':
-                #     imu_queue.put(data[2])
-                # response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-                response = "{}: {}".format(cur_thread.name, data[2])
-                # todo add function to change json
-                self.request.sendall(response.encode('utf-8'))
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
 
 
 class Game_Engine(threading.Thread):
@@ -137,6 +96,7 @@ class Game_Engine(threading.Thread):
     def eval_check(self, player_State):
         pass
 
+
 # MQTT Client to send data to AWS IOT Core
 class MQTT_Client(threading.Thread):
     def __init__(self, pub_topic, sub_topic, client_id, group) -> None:
@@ -166,80 +126,9 @@ class MQTT_Client(threading.Thread):
         if message.payload == b'grenade':
             # to update grenade damage for player 2
             action_queue.put('grenade_p2_hits') 
-        
-# Client to send data to the Evaluation Server
-class Evaluation_Client(threading.Thread):
-    
-    IV = b'PLSPLSPLSPLSWORK'
-    KEY = b'PLSPLSPLSPLSWORK'
-
-    def __init__(self, ip, port, group) -> None:
-        super().__init__()
-        self.eval_ip = gethostbyname(ip)
-        self.eval_port = port
-        self.group = group
-        
-        try:
-            self.clientSocket = socket(AF_INET, SOCK_STREAM)
-            self.clientSocket.connect((self.eval_ip, self.eval_port))
-            print('Connected to Evaluation Server', self.eval_ip, self.eval_port)
-        except:
-            print('Failed to connect to Evaluation Server', self.eval_ip, self.eval_port)
-            self.clientSocket = None
-    
-
-    def run(self):
-        f = open('test.json')
-        j = json.load(f)
-        for _ in range(10):
-            self.send(json.dumps(j))
-            self.receive()   
-        self.close()
-
-
-    @staticmethod
-    def AES_Cipher():
-        return AES.new(Evaluation_Client.KEY, AES.MODE_CBC, Evaluation_Client.IV)
-
-    def send(self, message):
-        if self.clientSocket is not None:
-            try:
-                encryted_message = self.encrypt_AES(message)
-                len_info = str(len(encryted_message)) + "_"
-                # send len_
-                self.clientSocket.send(len_info.encode("utf-8"))
-                self.clientSocket.send(encryted_message)
-                print('Sent message to Evaluation Server', self.eval_ip, self.eval_port, message)
-            except:
-                print('Failed to send message to Evaluation Server', self.eval_ip, self.eval_port, message)
-                self.clientSocket = None
-    
-    def receive(self):
-        if self.clientSocket is not None:
-            try:
-                recv_message = self.clientSocket.recv(2048).decode("utf-8")
-                print('Received message from Evaluation Server', self.eval_ip, self.eval_port, recv_message)
-            except:
-                print('Failed to receive message from Evaluation Server', self.eval_ip, self.eval_port)
-                self.clientSocket = None
-
-    def close(self):
-        if self.clientSocket is not None:
-            self.clientSocket.close()
-            print('Closed connection to Evaluation Server', self.eval_ip, self.eval_port)
-    
-    def encrypt_AES(self, string):    
-        msg = pad(string.encode("utf-8"), AES.block_size)
-        encrypted_text = Evaluation_Client.AES_Cipher().encrypt(msg)
-        ciphertext = base64.b64encode(self.IV + encrypted_text)
-        return ciphertext
 
 
 def main():
-    eval_client = Evaluation_Client('localhost', 11001, 2)
-    eval_client.daemon = True
-    eval_client.start()
-
     game_engine = Game_Engine()
     game_engine.daemon = True
     game_engine.start()
@@ -247,13 +136,6 @@ def main():
     mqtt = MQTT_Client('cg4002/gamestate', 'cg4002/visualiser', 'testpc', 2)
     mqtt.daemon = True
     mqtt.start()
-
-    HOST, PORT = "localhost", 11000
-    server = ThreadedTCPServer((HOST, PORT), Relay_Server)
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
 
     mqtt.client.loop_forever()
 
