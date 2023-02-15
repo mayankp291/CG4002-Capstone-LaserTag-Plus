15bd19b6e62adaffe08224b0a2e6ea93ea721f99
mathanmahe <e0407484@u.nus.edu>
2023-02-15 11:07:43 +0700

    Sample IMU data

--- InternalComms/beetle.ino
+++ InternalComms/beetle.ino
@@ -11,6 +11,8 @@ bool hasHandshake = false;
 byte sequenceNo;
 byte packetCount;
 
+int randomNumber;
+
 CRC8 crc;
 
 #define SYN_PACKET 'S'
@@ -27,13 +29,13 @@ uint8_t calculateCRC8(uint8_t *data, int len) {
 }
 
 struct AcknowledgementPacket {
-    byte typeOfPacket = "A";
+    byte typeOfPacket;
     byte padding[18];
-    byte checkSum = 'A';
+    byte crcCheck;
 } ackPacket;
 
 struct DatagramPacket {
-    byte typeOfPacket = 'M'; // motion
+    byte typeOfPacket; // motion
     byte deviceID;
     int16_t accX;
     int16_t accY;
@@ -45,12 +47,17 @@ struct DatagramPacket {
     byte crcCheck;
 } ;
 
-void sendACKPacket(char packetType) {
-    Serial.write(ACK_PACKET);
-    crc.add(ACK_PACKET);
-    Serial.write(crc.getCRC());
 
-    crc.restart();
+void sendACKPacket() {
+    AcknowledgementPacket ackPacket;
+    ackPacket.typeOfPacket = (byte) 'A';
+    Serial.write((byte *)&ackPacket, sizeof(ackPacket));
+//    Serial.write(ACK_PACKET);
+//    crc.add(ACK_PACKET);
+//    Serial.write(crc.getCRC());
+    ackPacket.padding[18] = {0};
+    ackPacket.crcCheck = 1;
+//    crc.restart();
 }
 
 void sendDataPacket() {
@@ -65,13 +72,16 @@ void setup(void) {
   hasHandshake = false;
   sequenceNo = 0;
   packetCount = 0;
+
+  // generating random numbers
+  randomNumber = random(500);
 }
 
 void doHandshake() {
   byte packetType = Serial.read();
   if (packetType == SYN_PACKET)
   {
-        sendACKPacket(ACK_PACKET);
+        sendACKPacket();
         // break;
   }
   else if (packetType == ACK_PACKET)
@@ -80,9 +90,66 @@ void doHandshake() {
   }
 }
 
+void getSensorReading() {
+    accX = random(500);
+    accY = random(500);
+    accZ = random(500);
+    gyrX = random(500);
+    gyrY = random(500);
+    gyrZ = random(500);
+}
+
+void sendSensorReading() {
+    DatagramPacket motionPacket;
+    motionPacket.typeOfPacket = (byte) 'M';
+    motionPacket.deviceID = 5;
+    motionPacket.accX = accX;
+    motionPacket.accY = accY;
+    motionPacket.accZ = accZ;
+    motionPacket.gyrX = gyrX;
+    motionPacket.gyrY = gyrY;
+    motionPacket.gyrZ = gyrZ;
+//    motionPacket.deviceID = 5;
+//    motionPacket.accX = 11;
+//    motionPacket.accY = 11;
+//    motionPacket.accZ = 11;
+//    motionPacket.gyrX = 11;
+//    motionPacket.gyrY = 11;
+//    motionPacket.gyrZ = 11;
+
+    motionPacket.padding[5] = {0};
+    motionPacket.crcCheck = 1;
+
+    Serial.write((byte *)&motionPacket, sizeof(motionPacket));
+}
+
+boolean hasSent = false;
+
+int count = 0;
+
 void loop(void) {
    if(Serial.available()) {
-    char serialRead = Serial.read();
-    Serial.println(serialRead);
+       if(hasHandshake == false) {
+            char serialRead = Serial.read();
+            // Serial.println(serialRead);
+            if (serialRead == 'S') {
+//              Serial.write('A');
+                sendACKPacket();
+            }
+            else if(serialRead == 'A') {
+              hasHandshake = true;
+
+            }
+       }
+
+       if(hasHandshake == true) {
+//          count<=5 &&
+           delay(2000);
+           getSensorReading();
+           sendSensorReading();
+           hasSent = true;
+           count++;
+       }
    }
+
 }
