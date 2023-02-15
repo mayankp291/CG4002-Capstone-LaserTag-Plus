11ad6847999dda14ae56e3185e354b312973a90f
mathanmahe <e0407484@u.nus.edu>
2023-02-15 11:06:46 +0700

    Protocol Completed

--- InternalComms/comms.py
+++ InternalComms/comms.py
@@ -1,12 +1,18 @@
 import multiprocessing as mp
 import os
+import signal
 from socket import *
+import sys
+import struct
 import threading
 from crccheck.crc import Crc8
 
 from bluepy.btle import DefaultDelegate, Peripheral, Scanner, BTLEDisconnectError
 
 # the peripheral class is used to connect and disconnect
+
+# timeouts in seconds
+CONNECTION_TIMEOUT = 3
 
 Service_UUID =  "0000dfb0-0000-1000-8000-00805f9b34fb"
 Characteristic_UUID = "0000dfb1-0000-1000-8000-00805f9b34fb"
@@ -18,11 +24,11 @@ Characteristic_UUID = "0000dfb1-0000-1000-8000-00805f9b34fb"
 
 macAddresses = {
     1: "D0:39:72:BF:BF:BB", #imu1
-    2: "D0:39:72:BF:C6:07", #gun1
-    3: "D0:39:72:BF:BF:DD", #vest1
-    4: "",
-    5: "D0:39:72:BF:C8:CF", #gun2
-    6: ""
+    2: "D0:39:72:BF:C6:07", #VEST1
+    3: "D0:39:72:BF:C3:BF", #GUN1
+    4: "D0:39:72:BF:C8:A8", #IMU2
+    5: "D0:39:72:BF:BF:DD", #vest2
+    6: "D0:39:72:BF:C8:CF" #gun2
 }
 
 DATA_PACKET_SIZE = 20
@@ -32,11 +38,11 @@ ACK_FLAGS = [False] * 6
 
 # Device IDs
 IMU_PLAYER_1 = 1
-GUN_PLAYER_1 = 2
-VEST_PLAYER_1 = 3
+VEST_PLAYER_1= 2
+GUN_PLAYER_1 = 3
 IMU_PLAYER_2 = 4
-GUN_PLAYER_2 = 5
-VEST_PLAYER_2 = 6
+VEST_PLAYER_2 = 5
+GUN_PLAYER_2 = 6
 
 SYN_PACKET = 'S'
 ACK_PACKET = 'A'
@@ -47,30 +53,124 @@ HEALTH_PACKET = 'H'
 
 # each beetle has a delegate to handle BLE transactions
 class MyDelegate(DefaultDelegate):
-    def __init__(self, playerId, deviceId, dataBuffer, lock, receivingBuffer):
+    def __init__(self, playerId, deviceId, dataBuffer, lock, receivingBuffer, hasHandshaken, serialSvc, serialChar):
         DefaultDelegate.__init__(self)
         self.playerId = playerId
         self.deviceId = deviceId
         self.lock = lock
-        self.hasHandshaken = False
+        self.hasHandshaken = hasHandshaken
         self.dataBuffer = dataBuffer
         self.receivingBuffer = receivingBuffer
+        self.serialSvc = serialSvc
+        self.serialChar = serialChar
+        self.motionPacketsCount = 0
+        self.gunPacketsCount = 0
+        self.fragPacketsCount = 0
 
     def sendAckPacket(self):
         self.serialChar.write(bytes("A", "utf-8"))
 
     def handleAckPacket(self):
+        ACK_FLAGS[self.deviceId] = True
+        print("received ack from beetle")
+        self.serialChar.write(bytes('A', encoding="utf-8"))
+        print("HandshakeCompleted")
         self.sendAckPacket()
         self.hasHandshaken = True
 
     def handleNotification(self, cHandle, data):
         self.receivingBuffer += data
+        if len(self.receivingBuffer) >=20:
+            # print("Data received from beetle: ", self.receivingBuffer)
+            dataPacket = self.receivingBuffer[0:20]
+            unpackedPacket = ()
+            expectedPacketFormat = ("bb6h5xb")
+            unpackedPacket = struct.unpack_from(expectedPacketFormat, dataPacket, 0)
+            # dataPacket = dataPacket[::-1]
+            # print(unpackedPacket)
+            packetType = chr(unpackedPacket[0])
+            print("packetType, deviceId, length: ", packetType , "," ,self.deviceId, ",", len(self.receivingBuffer))
+            if packetType == 'A':
+                self.handleAckPacket()
+            if packetType == 'M':
+                sendData = {
+                    "playerID": self.playerId,
+                    "beetleID": self.deviceId,
+                    "motionData": {
+                        "aX": unpackedPacket[2],
+                        "aY": unpackedPacket[3],
+                        "aZ": unpackedPacket[4],
+                        "gX": unpackedPacket[5],
+                        "gY": unpackedPacket[6],
+                        "gZ": unpackedPacket[7]
+                    }
+                }
+                self.motionPacketsCount += 1
+                print("MotionPacketsCount: ", self.motionPacketsCount)
+                print(sendData)
+                self.lock.acquire()
+                self.dataBuffer.put(sendData)
+                self.lock.release()
+            if packetType == 'B' or packetType == 'H':
+                expectedPacketFormat = ("bb?16xb")
+                self.gunPacketsCount += 1
+                print("GunPacketsCount: ", self.gunPacketsCount)
+                unpackedPacket = struct.unpack_from(expectedPacketFormat, dataPacket, 0)
+                sendData = {
+                    "playerID": self.playerId,
+                    "beetleID": self.deviceId,
+                    "hit": unpackedPacket[2],
+                }
+                print(sendData)
+                self.lock.acquire()
+                self.dataBuffer.put(sendData)
+                self.lock.release()
+                self.sendAckPacket()
+            self.receivingBuffer = b''
+        else:
+            self.fragPacketsCount += 1
+            self.receivingBuffer = self.receivingBuffer + data
+            if len(self.receivingBuffer) == 20:
+                self.handleNotification(None, self.receivingBuffer)
+            self.receivingBuffer = b''
+            print("Fragmented Packets Count:", self.fragPacketsCount)
+
+
+
+
+    def ohandleNotification(self, cHandle, data):
+        self.receivingBuffer += data
         print("Data received from beetle: ", self.receivingBuffer)
-        if (len(self.receivingBuffer)) == 1 and self.receivingBuffer == b'A':
+        if (len(self.receivingBuffer)) == 1 and self.receivingBuffer == b'A' and not ACK_FLAGS[self.deviceId]:
             # global beetleAck
             # beetleAck = True
             ACK_FLAGS[self.deviceId] = True
             self.receivingBuffer = b'' # reset the data
+
+        if ACK_FLAGS[self.deviceId] and len(self.receivingBuffer) >1:
+            dataPacket = self.receivingBuffer[0:20]
+            unpackedPacket = ()
+            # expectedPacketFormat = (
+            #     'b'
+            #     'b'
+            #     'h'
+            #     'h'
+            #     'h'
+            #     'h'
+            #     'h'
+            #     'h'
+            #     'x'
+            #     'b'
+            # )
+            expectedPacketFormat = ("bb6hxb")
+            unpackedPacket = struct.unpack_from(expectedPacketFormat, dataPacket, 0)
+            # dataPacket = dataPacket[::-1]
+            print(unpackedPacket)
+            # packetType = struct.unpack('b', dataPacket[0])
+            # deviceId = struct.unpack('i', dataPacket[1])
+            # print(self.receivingBuffer)
+            # print(packetType, deviceId)
+            self.receivingBuffer = b''
         self.receivingBuffer = b''
 
 
@@ -91,26 +191,31 @@ class BeetleConnectionThread:
         self.serialSvc = None
         self.serialChar = None
         self.receivingBuffer = receivingBuffer
+        self.hasHandshaken = False
 
     def writetoBeetle(self):
         pass
 
     def openBeetleConnection(self):
-        while 1:
-            try:
-                self.dev = Peripheral(self.macAddress)
-                print("Connected to Beetle: ", self.macAddress)
-                deviceDelegate = MyDelegate(self.playerId, self.beetleId, self.dataBuffer, self.lock, self.receivingBuffer)
-                self.dev.setDelegate(deviceDelegate)
-                self.serialSvc = self.dev.getServiceByUUID(Service_UUID)
-                self.serialChar = self.serialSvc.getCharacteristics(Characteristic_UUID)[0]
-                break
-            except BTLEDisconnectError:
-                print("Connection failed")
-                return
+        # while True:
+        try:
+            self.dev = Peripheral(self.macAddress)
+            print("Connected to Beetle: ", self.macAddress)
+            self.serialSvc = self.dev.getServiceByUUID(Service_UUID)
+            self.serialChar = self.serialSvc.getCharacteristics(Characteristic_UUID)[0]
+            deviceDelegate = MyDelegate(self.playerId, self.beetleId, self.dataBuffer, self.lock,
+                                        self.receivingBuffer, self.hasHandshaken, self.serialSvc, self.serialChar)
+            self.dev.withDelegate(deviceDelegate)
+            return True
+            # break
+        except BTLEDisconnectError:
+            print("Connection failed")
+            return False
+            # return
 
-    def startThreeWayHandshake(self):
-        hasHandshake = False
+
+    def startThreeWayHandshake(self, hasHandshake):
+
         while not hasHandshake:
             self.dev.waitForNotifications(1.0)
 
@@ -125,10 +230,49 @@ class BeetleConnectionThread:
                 print("HandshakeCompleted")
         return hasHandshake
 
+    def sendSynMessage(self):
+        self.dev.waitForNotifications(1.0)
+        if not SYN_FLAGS[self.beetleId]:
+            print("sending syn to beetle")
+            self.serialChar.write(bytes('S', encoding="utf-8"))
+            SYN_FLAGS[self.beetleId] = True
+
     def executeCommunications(self):
         # connect to beetle
-        self.openBeetleConnection()
-        self.startThreeWayHandshake()
+        hasHandshake = False
+        isConnected = False
+        while True:
+            try:
+                if not hasHandshake:
+                    if not isConnected:
+                        isConnected = self.openBeetleConnection()
+                    # hasHandshake = self.startThreeWayHandshake(hasHandshake)
+                    self.sendSynMessage()
+                if not self.dev.waitForNotifications(CONNECTION_TIMEOUT):
+                    self.hasHandshaken = False
+                    isConnected = False
+                    hasHandshake = False
+                    SYN_FLAGS[self.beetleId] = False
+                    ACK_FLAGS[self.beetleId] = False
+                    self.dev.disconnect()
+                    # continue
+            except KeyboardInterrupt:
+                self.dev.disconnect()
+                self.hasHandshaken = False
+                isConnected = False
+                hasHandshake = False
+                SYN_FLAGS[self.beetleId] = False
+                ACK_FLAGS[self.beetleId] = False
+            except BTLEDisconnectError:
+                print("Device Disconnected")
+                self.hasHandshaken = False
+                isConnected = False
+                hasHandshake = False
+                SYN_FLAGS[self.beetleId] = False
+                ACK_FLAGS[self.beetleId] = False
+
+            except Exception:
+                pass
 
 
 def executeThreads():
@@ -144,20 +288,20 @@ def executeThreads():
     IMU1_Beetle = BeetleConnectionThread(1, IMU_PLAYER_1, macAddresses.get(1), dataBuffer, lock)
     IMU1_Thread = threading.Thread(target=IMU1_Beetle.executeCommunications())
 
-    Gun1_Beetle = BeetleConnectionThread(1, GUN_PLAYER_1, macAddresses.get(2), dataBuffer, lock)
+    Gun1_Beetle = BeetleConnectionThread(1, GUN_PLAYER_1, macAddresses.get(3), dataBuffer, lock)
     Gun1_Thread = threading.Thread(target=Gun1_Beetle.executeCommunications())
 
-    Vest1_Beetle = BeetleConnectionThread(1, VEST_PLAYER_1, macAddresses.get(3), dataBuffer, lock)
+    Vest1_Beetle = BeetleConnectionThread(1, VEST_PLAYER_1, macAddresses.get(2), dataBuffer, lock)
     Vest1_Thread = threading.Thread(target=Vest1_Beetle.executeCommunications())
 
     # Player 2
     IMU2_Beetle = BeetleConnectionThread(2, IMU_PLAYER_2, macAddresses.get(4), dataBuffer, lock)
     IMU2_Thread = threading.Thread(target=IMU2_Beetle.executeCommunications())
 
-    Gun2_Beetle = BeetleConnectionThread(2, GUN_PLAYER_2, macAddresses.get(5), dataBuffer, lock)
+    Gun2_Beetle = BeetleConnectionThread(2, GUN_PLAYER_2, macAddresses.get(6), dataBuffer, lock)
     Gun2_Thread = threading.Thread(target=Gun2_Beetle.executeCommunications())
 
-    Vest2_Beetle = BeetleConnectionThread(2, VEST_PLAYER_2, macAddresses.get(6), dataBuffer, lock)
+    Vest2_Beetle = BeetleConnectionThread(2, VEST_PLAYER_2, macAddresses.get(5), dataBuffer, lock)
     Vest2_Thread = threading.Thread(target=Vest2_Beetle.executeCommunications())
 
     IMU1_Thread.start()
@@ -178,10 +322,44 @@ def executeThreads():
 
 
 if __name__ == '__main__':
-    lock = mp.Lock()
+    try:
+        lock = mp.Lock()
 
-    # using a multiprocessing queue FIFO
-    dataBuffer = mp.Queue()
-    receivingBuffer = b''
-    Gun2_Beetle = BeetleConnectionThread(2, GUN_PLAYER_2, macAddresses.get(5), dataBuffer, lock, receivingBuffer)
-    Gun2_Beetle.executeCommunications()
+        # using a multiprocessing queue FIFO
+        dataBuffer = mp.Queue()
+        receivingBuffer1 = b''
+        receivingBuffer2 = b''
+        receivingBuffer3 = b''
+        # IMU2_Beetle = BeetleConnectionThread(2, IMU_PLAYER_2, macAddresses.get(4), dataBuffer, lock, receivingBuffer)
+        # IMU2_Beetle.executeCommunications()
+
+        # Devices 234
+        Gun1_Beetle = BeetleConnectionThread(1, GUN_PLAYER_1, macAddresses.get(3), dataBuffer, lock, receivingBuffer1)
+        Gun1_Thread = threading.Thread(target=Gun1_Beetle.executeCommunications, args = ())
+
+        Vest1_Beetle = BeetleConnectionThread(1, VEST_PLAYER_1, macAddresses.get(2), dataBuffer, lock, receivingBuffer2)
+        Vest1_Thread = threading.Thread(target=Vest1_Beetle.executeCommunications, args = ())
+
+        # Player 2
+        IMU2_Beetle = BeetleConnectionThread(2, IMU_PLAYER_2, macAddresses.get(4), dataBuffer, lock, receivingBuffer3)
+        IMU2_Thread = threading.Thread(target=IMU2_Beetle.executeCommunications, args = ())
+
+        # Gun1_Thread.daemon = True
+        # Vest1_Thread.daemon = True
+        # IMU2_Thread.daemon = True
+
+        Gun1_Thread.start()
+        Vest1_Thread.start()
+        IMU2_Thread.start()
+
+        Gun1_Thread.join()
+        Vest1_Thread.join()
+        IMU2_Thread.join()
+
+        # while True: time.sleep(100)
+
+        # signal.pause()
+
+    except (KeyboardInterrupt, SystemExit):
+        print("Ended Comms")
+        # sys.exit()
