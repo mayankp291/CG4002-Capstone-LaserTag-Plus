c3a78fc00619bdabd94bdb54857f83f2ff1bcd43
JL <tha.junlim@gmail.com>
2023-03-12 20:18:45 +0800

    Update sliding window algo

--- AI/slidingwindow.py
+++ AI/slidingwindow.py
@@ -1,66 +1,91 @@
 from collections import deque
-
+import numpy as np
 
 class SlidingWindow:
-    window_size = 20
-    acc_x = deque()
-    acc_y = deque()
-    acc_z = deque()
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.data = deque(maxlen=window_size)
+        self.acc_mean = np.zeros(3)
+        self.acc_std = np.zeros(3)
+        self.gyro_mean = np.zeros(3)
+        self.gyro_std = np.zeros(3)
+        
 
-    def __int__(self):
-        """
-        Keep a window of size determined by the size parameter
-        :return: An object of SlidingWindow
-        """
-        self.acc_x = deque()
-        self.acc_y = deque()
-        self.acc_z = deque()
+    def fill(self, new_data):
+        for d in new_data:
+            self.data.append(d)
+        
+        # update mean and std
+        self.acc_mean = np.mean(np.array(self.data)[:, :3], axis=0)
+        self.acc_std = np.std(np.array(self.data)[:, :3], axis=0)
+        self.gyro_mean = np.mean(np.array(self.data)[:, 3:], axis=0)
+        self.gyro_std = np.std(np.array(self.data)[:, 3:], axis=0)
+            
 
-    def update_window_size(self, size):
-        """
-        Change the size of the window, will clear the window as well
-        :param size: The new size required
-        :return: None
-        """
-        self.window_size = size
+    def clear(self):
+        self.data.clear()
+        self.acc_mean = np.zeros(3)
+        self.acc_std = np.zeros(3)
+        self.gyro_mean = np.zeros(3)
+        self.gyro_std = np.zeros(3)
+        
 
-    def update_window(self, raw_data):
-        """
-        Update the current window by appending the new data and removing the old data
-        :param raw_data: Expect an array of size 3 which corresponds to accelerometer reading of a single data point
-        :return: None
-        """
-        if len(self.acc_x) == self.window_size:
-            self.acc_x.popleft()
-            self.acc_y.popleft()
-            self.acc_z.popleft()
-        self.acc_x.append(raw_data[0])
-        self.acc_y.append(raw_data[1])
-        self.acc_z.append(raw_data[2])
+    def add_new_value(self, new_value):
+        self.data.popleft()
+        self.data.append(new_value)
+        
+        # update mean and std
+        self.acc_mean = np.mean(np.array(self.data)[:, :3], axis=0)
+        self.acc_std = np.std(np.array(self.data)[:, :3], axis=0)
+        self.gyro_mean = np.mean(np.array(self.data)[:, 3:], axis=0)
+        self.gyro_std = np.std(np.array(self.data)[:, 3:], axis=0)
+        
 
-    def get_curr_value(self):
-        """
-        Get the threshold value of the current sliding window
-        :return: The threshold value of the current sliding window, or 0 if window is empty
-        """
-        if len(self.acc_x) == 0:
-            return 0
-        return max(self.acc_x) - min(self.acc_x) + max(self.acc_y) - min(self.acc_y) + max(self.acc_z) - min(self.acc_z)
+    def remove_next_value(self):
+        self.data.popleft()
+        
+        # update mean and std
+        self.acc_mean = np.mean(np.array(self.data)[:, :3], axis=0)
+        self.acc_std = np.std(np.array(self.data)[:, :3], axis=0)
+        self.gyro_mean = np.mean(np.array(self.data)[:, 3:], axis=0)
+        self.gyro_std = np.std(np.array(self.data)[:, 3:], axis=0)
 
-    def clear_window(self):
-        """
-        Remove all data stored in the window
-        :return: None
-        """
-        self.acc_x.clear()
-        self.acc_y.clear()
-        self.acc_z.clear()
+        
+    def is_start_of_move(self):
+        # define threshold values as 2 standard deviations away from the mean
+        acc_thresh = 2 * self.acc_std
+        gyro_thresh = 2 * self.gyro_std
+        
+        # compare each data point in window to threshold
+        for j in range(self.window_size):
+            acc_vals = np.array(self.data)[j, :3]
+            gyro_vals = np.array(self.data)[j, 3:]
+            
+            if ((acc_vals > self.acc_mean + acc_thresh).all() and (gyro_vals > self.gyro_mean + gyro_thresh).all()) or ((acc_vals < self.acc_mean - acc_thresh).all() and (gyro_vals < self.gyro_mean - gyro_thresh).all()):
+                # potential start of move action identified
+                # check next few data points to confirm start of move action
+                for k in range(j+1, j+4):
+                    try:
+                        next_acc_vals = np.array(self.data)[k, :3]
+                        next_gyro_vals = np.array(self.data)[k, 3:]
+                    except IndexError:
+                        # if index is out of range, move to next window
+                        break
 
-    def fill_window(self, raw_data):
-        """
-        Fill the window by the raw data
-        :param raw_data: Expect a raw data contains at least self.window_size data points
-        :return: None
-        """
-        for datapoint in raw_data[:5]:
-            self.update_window(datapoint[:3])
+                    if not ((next_acc_vals > self.acc_mean + acc_thresh).all() and (next_gyro_vals > self.gyro_mean + gyro_thresh).all() or (next_acc_vals < self.acc_mean - acc_thresh).all() and (next_gyro_vals < self.gyro_mean - gyro_thresh).all()):
+                        # not the start of move action, move to next window
+                        break
+                else:
+                    # confirmed start of move action
+                    return True
+        return False
+
+
+    def get_window_matrix(self):    
+        window_matrix = np.array(self.data)
+        # pad with zeros if data is less than window size
+        if window_matrix.shape[0] < self.window_size:
+            pad_size = self.window_size - window_matrix.shape[0]
+            padding = np.zeros((pad_size, window_matrix.shape[1]))
+            window_matrix = np.concatenate((padding, window_matrix), axis=0)
+        return window_matrix.T
