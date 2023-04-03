4a3553f8da3ffa96295c4e097d5e8ccc49a3a4a0
Mayank Panjiyara <mayankp@u.nus.edu>
2023-04-04 00:36:22 +0800

    add bash scripts to ssh and start int comms

--- ssh.sh
+++ ssh.sh
@@ -3,6 +3,16 @@
 # Set variables for the SSH command
 jump_host="mayankp@stu.comp.nus.edu.sg"
 target_host="xilinx@192.168.95.235"
+set pass "plsdonthackus"
 
 # Run SSH command with jump host
 ssh -J $jump_host $target_host
+
+expect "$"
+send "sudo su - root"
+
+expect "[sudo] password for xilinx: " 
+send "$pass"
+
+expect "$ "
+send cd ../../home/xilinx
---
+++ start_intcomms.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/CG4002-Capstone
+source venv/bin/activate
+cd InternalComms
+python comms_integration_test.py
